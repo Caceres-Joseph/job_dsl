5f21d8d5cb512844c4e7df595ad835bd3a99e199
Caceres-Joseph <josephccaceres@gmail.com>
2019-10-17 15:46:47 -0600

    Update job-dsl-simple.groovy

--- job-dsl-simple.groovy
+++ job-dsl-simple.groovy
@@ -1,12 +1,4 @@
 job('Example') {
-    scm {
-        git('git://github.com/quidryan/aws-sdk-test.git')
-        //github('jenkinsci/job-dsl-plugin', 'master')
-    }
-    triggers {
-        scm('* * * * *')
-        //cron("@hourly")
-    }
     steps {
         shell("echo 'Hello World'")
     }
